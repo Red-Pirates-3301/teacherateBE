8e2944e6a48f29748e68e140b808c2cf0f190cf7
Fouad dim <89441786+FouadDim@users.noreply.github.com>
2023-03-04 13:05:02 +0200

    Update urls.py

--- addAccount/urls.py
+++ addAccount/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path("/get_csrf", generate_csrf_token, name="generate_csrf_token"),
     path("/add_account", addAccount, name="addAccount"),
     path("/add_rating", addRating, name="addRating"),
-    path("/get_ip", get_client_ip, name="get_client_ip"),
     path("/retrieve_account", retrieve, name="retrieve")
 ]
